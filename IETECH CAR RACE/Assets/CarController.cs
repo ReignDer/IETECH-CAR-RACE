cdbde1892884b98c74538c1d7605d05cd018554e
Mathieu Marc Pobre <mathieu_marc_pobre@dlsu.edu.ph>
2024-11-08 23:20:55 +0800

    commit

--- IETECH CAR RACE/Assets/CarController.cs
+++ IETECH CAR RACE/Assets/CarController.cs
@@ -7,25 +7,38 @@ public class CarController : MonoBehaviour
     private float horizontalInput, verticalInput;
     private float currentSteerAngle, currentbreakForce;
     public float MPH;
+    private float downForce = 50f;
     private bool isBreaking;
+    private GameObject centerOfMass;
     private Rigidbody rigidbody;
     // Settings
     [SerializeField] private float motorForce, breakForce, maxSteerAngle;
+    [SerializeField] private float maxSpeedMph;
 
     // Wheel Colliders
     [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
     [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
 
+    private WheelCollider[] wheelList = new WheelCollider[4];
+
     // Wheels
     [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
     [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
 
+    private void Start()
+    {
+        wheelList[0] = frontLeftWheelCollider;
+        wheelList[1] = rearLeftWheelCollider;
+        wheelList[2] = frontRightWheelCollider;
+        wheelList[3] = rearRightWheelCollider;
+        GetObject();
+    }
     private void FixedUpdate()
     {
         GetInput();
-        GetObject();
         HandleMotor();
         HandleSteering();
+        ApplyDownForce();
         UpdateWheels();
     }
 
@@ -43,15 +56,26 @@ public class CarController : MonoBehaviour
 
     private void GetObject() { 
         rigidbody = GetComponent<Rigidbody>();
+        centerOfMass = GameObject.Find("CenterOfMass");
+        rigidbody.centerOfMass = centerOfMass.transform.localPosition;
     }
 
     private void HandleMotor()
     {
-        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
-        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        for (int i = 0; i < wheelList.Length; i++)
+        {
+            wheelList[i].motorTorque = verticalInput * (motorForce / 4);
+        }
+        //frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        //frontRightWheelCollider.motorTorque = verticalInput * motorForce;
         currentbreakForce = isBreaking ? breakForce : 0f;
-        MPH = rigidbody.velocity.magnitude * 2.234f;
+        MPH = rigidbody.velocity.magnitude * 2.23694f;
         ApplyBreaking();
+
+        if (MPH > maxSpeedMph)
+        {
+            rigidbody.velocity = rigidbody.velocity.normalized * (maxSpeedMph / 2.23694f); 
+        }
     }
 
     private void ApplyBreaking()
@@ -68,7 +92,10 @@ public class CarController : MonoBehaviour
         frontLeftWheelCollider.steerAngle = currentSteerAngle;
         frontRightWheelCollider.steerAngle = currentSteerAngle;
     }
-
+    private void ApplyDownForce()
+    {
+        rigidbody.AddForce(-transform.up * downForce * rigidbody.velocity.magnitude);
+    }
     private void UpdateWheels()
     {
         UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
--- IETECH CAR RACE/Assets/GameManager.cs
+++ IETECH CAR RACE/Assets/GameManager.cs
@@ -1,15 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    [SerializeField]
+    private Text timeText;
+
+    [SerializeField]
+    private Text speedText;
+    [SerializeField]
+    private Text lapText;
+    private int lap = 0;
+
+    [SerializeField]
+    private Text finalTimeText;
+
+    public GameObject endPanel;
+
     public CarController MPH;
     public GameObject needle;
     private float minSpeed = 209f, maxSpeed = -30.89f;
     private float currentSpeed;
+    private bool startTimer = false;
+    private float timeElapsed;
 
     public float vehicleSpeed;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this.gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +46,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         vehicleSpeed = MPH.MPH;
+        if (startTimer)
+        {
+            Timer();
+        }
     }
 
     private void FixedUpdate()
@@ -28,8 +58,34 @@ public class GameManager : MonoBehaviour
     }
     private void updateNeedle()
     {
+        float speed = Mathf.Round(vehicleSpeed);
+        speedText.text = string.Format("{0} MPH", speed); 
         currentSpeed = minSpeed - maxSpeed;
         float temp = vehicleSpeed / 260;
         needle.transform.eulerAngles = new Vector3 (0, 0, (minSpeed - temp * currentSpeed));
+    }
+    public void StartTimer()
+    {
+        startTimer = true;
+    }
+    public void EndTimer()
+    {
+        startTimer = false;
+        lap++;
+        lapText.text = string.Format("Lap {0}/1", lap);
+
+        finalTimeText.text = "Final Time: " + timeText.text ;
+        endPanel.SetActive(true);
+
+    }
+    private void Timer()
+    {
+        timeElapsed += Time.deltaTime;
+        float minutes = Mathf.FloorToInt(timeElapsed / 60f);
+        float seconds = Mathf.FloorToInt(timeElapsed % 60f);
+        int milliseconds = Mathf.FloorToInt((timeElapsed % 1) * 1000);
+
+        timeText.text = string.Format("{0:00}:{1:00}.{2:00}", minutes, seconds, milliseconds);
+  
     }
 }
